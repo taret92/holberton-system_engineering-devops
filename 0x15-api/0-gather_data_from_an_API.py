4f99f858b1b4cea9896a98c56c2aa7e44fe20d57
taret92 <ivancho92.10@hotmail.com>
2022-09-11 14:38:28 -0500

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 """
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress
+    Script that fetches the endpoint https://jsonplaceholder.typicode.com/
+    And process the data
 """
 import requests
 import sys
 
 
-if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-    name_user = '{}users/{}'.format(url, sys.argv[1])
-    r = requests.get(name_user)
-    r_json = r.json()
-    print("Employee {} is done with tasks".format(r_json.get('name')), end="")
-    r = requests.get(url + 'all', params={'userId': sys.argv[1]})
-    tasks = r.json()
-    task_list = []
-    for task in tasks:
-        if task.get('completed'):
-            task_list.append(task)
+if __name__ == '__main__':
+    id = int(sys.argv[1])
 
-    print("({}/{}):".format(len(task_list), len(tasks)))
-    for task in task_list:
-        print("\t {}".format(task.get("title")))
+    res = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(id)).json()
+    employee_name = res.get('name')
+    res = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
+    ).json()
+    total_tasks = len(res)
+    completed_tasks = len([task for task in res if task.get('completed')])
+    pending_tasks = "\t " + "\t ".join(
+        [str(task.get('title') + "\n")
+         for task in res if task.get('completed')]
+    )
+    print("Employee {} is \
+done with tasks({}/{}):\n{}".format(
+        employee_name, completed_tasks, total_tasks, pending_tasks), end="")
